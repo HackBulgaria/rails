5743f77519ea74d49f4b8d135e2a5dabb2b93cbb
Genadi Samokovarov <gsamokovarov@gmail.com>
2015-03-04 21:35:54 +0200

    Shortcut Puppet bootstrap if the proper version is present

--- puppet/bootstrap.sh
+++ puppet/bootstrap.sh
@@ -7,6 +7,11 @@
 PUPPET_VERSION=3.6.1-1ubuntu1
 PUPPET_REPO_URL="http://apt.puppetlabs.com/puppetlabs-release-${DISTRIB_CODENAME}.deb"
 
+# Skip processing if we already have the proper Puppet version.
+dpkg -s puppet | grep -q "Version: ${PUPPET_VERSION}" && {
+  exit 0
+}
+
 download_path=$(mktemp)
 wget --output-document="${download_path}" "${PUPPET_REPO_URL}" 2>/dev/null
 dpkg -i "${download_path}"
